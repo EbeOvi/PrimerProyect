c0141a1d5e1b5b986511f94a4b2a90fb740770cd
Rebeca Oviedo <ebeoviedo44@gmail.com>
2023-02-02 08:21:41 -0300

    primer escritura en el repositorio

---
+++ proyecto2/fuction.js
@@ -0,0 +1,6 @@
+
+document.getElementById('boton').onclick = function ()  
+{
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML= "Ser eficiente y eficas, cumplir de manera responsable en lo que se me requiera";
+} 
